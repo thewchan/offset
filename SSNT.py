81c07d27d38f07423c73af75ec9c7ce6e6e51a5b
Ron-Chang <aaron909979@gmail.com>
2019-02-09 12:31:46 -0800

    Edit SSNT

--- SSNT.py
+++ SSNT.py
@@ -1,10 +1,6 @@
 import os
 import subprocess
 
-
-# symbol = ":;<=>?@!\"#$%&'()*+,-./\n"
-# symbol = "\n"
-
 def check_dir():
     # Go to [export] in current folder, if it wasn't exist,then create it.
     try:
@@ -13,19 +9,6 @@ def check_dir():
         os.mkdir("export")
     else:
         os.chdir("..")
-
-# def opt_funcion(usrinput):
-#     while True:
-#         if usrinput == 1:
-#             # Convert input string
-#             convert(ori_string, shift_value)
-#         elif usrinput == 2:
-#             # Input Text file
-#         elif usrinput == 3:
-#             # input multiple files
-#             pass
-#         else:
-#             continue
 
 def im_port():
     # An input loop until the path is correct.
@@ -43,35 +26,7 @@ def im_port():
         except:
             # If it went wrong, go back and loop again.
             continue
-
-# def multi_im_port():
-#     data_list = []
-#     data_name_list = []
-#     file_path_list = []
-#     while True:
-#         print("Drag the file here or keyin the pathname.")
-#         print("Keyin 'q' to finish the input.")
-#         file_path = input(">>> ")
-#         if file_path in ["q","Q"]:
-#             for file_path in file_path_list:
-
-#                 try:
-#                     with open(file_path, "r") as file:
-#                         # read the file assign to the data
-#                         data = file.read()
-#                         data_list.append(data)
-#                         data_name_list.append(file_path.split("/")[-1])
-#                 except Exception as Error_info:
-#                     # Print Error informaiton..
-#                     print(Error_info)
-
-#             if len(data_list) > 0:
-#                 return data_list,data_name_list
-#             else:
-#                 file_path_list = []
-
-#         else:
-#             file_path_list.append(file_path)
+           file_path_list.append(file_path)
 
 def ex_port(data,name):
     check_dir()
@@ -92,18 +47,6 @@ def convert(ori_string,shift_value):
 
     print(new_string)
     return new_string
-
-    # def convert(ori_string,shift_value):
-
-    # new_string = ""
-    # for character in ori_string:
-    #     if character not in symbol:
-    #         character = chr(ord(character)+shift_value)
-    #     if character != "\n":
-    #         new_string += character
-
-    # print(new_string)
-    # return new_string
 
 def main():
     while True:
@@ -136,6 +79,7 @@ def main():
                         continue
 
         elif function_option == "2":
+            # Convert a Text
             ori_string = im_port()
 
             print("Input shift value:")
@@ -145,23 +89,19 @@ def main():
             ex_port(data,ori_string[1])
             #Reveal in Finder
             subprocess.call(["open","export"])
-            subprocess.call(["open","export/test_convert.txt"])
-
-        # elif function_option == "3":
-        #     ori_string = multi_im_port()[1]
-        #     print(ori_string)
-        #     #Reveal in Finder
-        #     subprocess.Popen("open export",shell=True)
 
         elif function_option == "w":
+            # Show Current working directory
             subprocess.call(["pwd"])
 
         elif function_option in ["q","Q","Quit","QUIT"]:
+            # Terminate the fuctiion
             terminate_info = "\n\t+"+"-"*33+"+"+"\n\t|  Function has been terminated!  |\n\t"+"+"+"-"*33+"+"
             print("\n"*20)
             exit(terminate_info)
         else:
-            print("\n----Error Input!----\n")
+            # Show Invalid Input!
+            print("\n----Invalid Input!----\n")
 
 if __name__ == "__main__":
     main()
